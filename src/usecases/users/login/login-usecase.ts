8f356c2df7ef3c54f0bbee8756f052a91e92bba7
Kaio Moreira <kaio-dev@outlook.com>
2023-09-01 13:24:35 -0300

    test: verify not return login

--- src/usecases/users/login/login-usecase.ts
+++ src/usecases/users/login/login-usecase.ts
@@ -34,14 +34,14 @@ export class LoginUseCase{
         if(!findUserExists){
             throw new CredentialsInvalidError()
         }
-        console.log('log1')
+        console.log('log1', findUserExists)
         // comparar senha
         const passwordMatch = await compare(password, findUserExists.password)
 
         if(!passwordMatch){
             throw new CredentialsInvalidError()
         }
-        console.log('log2')
+        console.log('log2', findUserExists)
        
         // Criar access token
         const accessToken = jwt.sign({}, env.JWT_SECRET_ACCESS_TOKEN, {
@@ -76,7 +76,7 @@ export class LoginUseCase{
             emailActive: findUserExists.emailActive,
             createdAt: findUserExists.createdAt,
         } as User
-
+        console.log('log3', findUserExists, accessToken, refreshToken)
         return {
             user,
             accessToken,
