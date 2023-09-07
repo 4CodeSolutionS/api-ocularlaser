49dc3596d4ba15b788576fa2721f8dfef4cf65be
Kaio Moreira <kaio-dev@outlook.com>
2023-09-06 22:55:48 -0300

    remove: factories invalids

--- src/usecases/factories/address/make-create-addresses-usecase.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
-import { CreateAddressUseCase } from "@/usecases/address/create/create-addresses-usecase";
-
-export async function makeCreateAddress(): Promise<CreateAddressUseCase> {
-    const addressRepository = new PrismaAddressesRepository();
-    const createAddressUsecase = new CreateAddressUseCase(addressRepository)
-
-    return createAddressUsecase
-}
--- src/usecases/factories/address/make-delete-addresses-usecase.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
-import { DeleteAddressUseCase } from "@/usecases/address/delete/delete-address-usecase";
-
-export async function makeDeleteAddress(): Promise<DeleteAddressUseCase> {
-    const addressRepository = new PrismaAddressesRepository();
-    const deleteAddressUseCase = new DeleteAddressUseCase(addressRepository)
-
-    return deleteAddressUseCase
-}
