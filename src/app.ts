641fb9afdc7972c14467ea0642513250a7156dd8
Kaio Moreira <kaio-dev@outlook.com>
2023-09-19 16:24:51 -0300

    alter: config cors for add methods

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ export const fastifyApp = fastify()
 fastifyApp.register(fastifyCors, {
     origin: true,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 })
 
 fastifyApp.register(multer.contentParser)
