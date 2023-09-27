a6779be20e68c4c993179f2516e45c0bfe45848d
Kaio Moreira <kaio-dev@outlook.com>
2023-09-27 17:19:31 -0300

    remove: webhooks for payments

--- src/http/controllers/payments/routes.ts
+++ src/http/controllers/payments/routes.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { ReceiveEventsPayments } from "./webhooks/receive-events-payments-controller";
 
 export async function paymentsRoutes(fastifyApp: FastifyInstance){
     // receive events payments
-    fastifyApp.post('/webhook', ReceiveEventsPayments)   
+    // fastifyApp.post('/webhook', ReceiveEventsPayments)   
 }
