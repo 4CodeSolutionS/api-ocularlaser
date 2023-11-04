ae63ca4500755911c3af01b1262612e3bed2b18f
Kaio Moreira <kaio-dev@outlook.com>
2023-11-04 07:28:12 -0300

    alter: receive event for catch originalValue

--- src/http/controllers/payments/events-webhook/events-webhook-payments-controller.ts
+++ src/http/controllers/payments/events-webhook/events-webhook-payments-controller.ts
@@ -51,7 +51,7 @@ export async function EventsWebHookPaymentsUseCases (request: FastifyRequest, re
                     customer,
                     value,
                     netValue,
-                    originalValue,
+                    originalValue: originalValue ? originalValue : undefined,
                     invoiceUrl,
                     externalReference,
                     paymentDate,
